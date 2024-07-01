e606bbbc5e247655dbb49b9f19b1049902767631
jb520 <jnavarro1995@gmail.com>
2024-07-01 11:55:16 -0500

    edit follow_request model

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -20,6 +20,6 @@
 #  fk_rails_...  (sender_id => users.id)
 #
 class FollowRequest < ApplicationRecord
-  belongs_to :recipient
-  belongs_to :sender
+  belongs_to :recipient, class_name: "User"
+  belongs_to :sender, class_name: "User"
 end
